877bf0952b61c4945474defce183b813e8f7b26c
Ebba Andersson <eeebbaandersson@gmail.com>
2026-01-31 16:19:57 +0100

    feat: add ability to add items to shopping cart

---
+++ src/main/java/com/example/shop/Item.java
@@ -0,0 +1,5 @@
+package com.example.shop;
+
+import java.math.BigDecimal;
+
+public record Item(String name, BigDecimal price) {}
--- src/test/java/com/example/ShoppingCartTest.java
+++ src/test/java/com/example/ShoppingCartTest.java
@@ -1,5 +1,13 @@
 package com.example;
 
+import com.example.shop.Item;
+import com.example.shop.ShoppingCart;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ShoppingCartTest {
 
     // Write test - test fails - write code - test passes - refactor
@@ -13,6 +21,21 @@ public class ShoppingCartTest {
 
     // --Tester--
     // Lägg till varor
+    @Test
+    void addItems_shouldBeStoredInShoppingCart() {
+        // Arrange
+        ShoppingCart cart = new ShoppingCart();
+        Item milk = new Item("Milk", new BigDecimal("16.0"));
+        Item coffee = new Item("Coffee", new BigDecimal("93.0"));
+
+        // Act
+        cart.addItem(milk);
+        cart.addItem(coffee);
+
+        // Assert - Förväntar oss  att shoppingCart ska innehålla 2 varor
+        assertThat(cart.getItems()).hasSize(2).containsExactly(milk, coffee);
+    }
+
     // Ta bort varor
     // Beräkna totalpris
     // Applicera rabatter
